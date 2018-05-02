ecd1b7d52f313f6c11fe8c29a580db32c6515079
graciany <gracianylebron@gmail.com>
2018-05-02 14:23:50 -0400

    Lexer reads methods and keywords correctly now.

--- sip_lex.py
+++ sip_lex.py
@@ -4,47 +4,100 @@
 # -----------------------------------------------------------------------------
 import ply.lex as lex
 import ply.yacc as yacc
+from ply.lex import TOKEN
+import re
 import sys
 
 # reserved Words
+# reserved = {
+#     'readImage': 'READ',
+#     'rotate': 'ROTATE',
+#     'resize': 'RESIZE',
+#     'translate': 'TRANSLATE',
+#     'enhance': 'ENHANCE',
+#     'sharpen': 'SHARPEN',
+#     'blur': 'BLUR',
+#     'denoise': 'DENOISE',
+#     'greyScale': 'GREY',
+#     'sepia': 'SEPIA',
+#     'getR': 'RED',
+#     'getG': 'GREEN',
+#     'getB': 'BLUE',
+#     'getEdges': 'EDGES',
+#     'segmentation': 'SEGMENTATION',
+#     'show': 'SHOW'
+#
+# }
+
 reserved = {
-    'readImage': 'READ',
-    'rotate': 'ROTATE',
-    'resize': 'RESIZE',
-    'translate': 'TRANSLATE',
-    'enhance': 'ENHANCE',
-    'sharpen': 'SHARPEN',
-    'blur': 'BLUR',
-    'denoise': 'DENOISE',
-    'greyScale': 'GREY',
-    'sepia': 'SEPIA',
-    'getR': 'RED',
-    'getG': 'GREEN',
-    'getB': 'BLUE',
-    'getEdges': 'EDGES',
-    'segmentation': 'SEGMENTATION',
-    'show': 'SHOW'
+    'METHOD_NP': ['greyScale', 'sepia', 'getR',
+                  'getG', 'getB', 'getEdges', 'segmentation'],
+    'METHOD_1P': ['enhance', 'sharpen', 'blur', 'denoise', 'rotate'],
+    'METHOD_2P': ['translate', 'resize'],
+    'METHOD_NO': ['readImage'],
+    'LEVEL': ['low', 'medium', 'high'],
+    'DIRECTION': ['right', 'left'],
+
 
 }
 
 # tokens
-tokens = (
+tokens = [
     'INT', 'FLOAT',
-    'EQUALS', 'ID', 'LBRACE', 'RBRACE', 'EQUALS', 'DOT',
-    'COMMA', 'LPAREN', 'RPAREN', 'STRING', 'METHOD'
-)
+    'EQUALS', 'ID', 'LCURLYB', 'RCURLYB', 'EQUALS', 'DOT',
+    'COMMA', 'LPAREN', 'RPAREN', 'STRING', 'METHOD', 'LIST', 'STRING'
+] + list(reserved)
 
-# Basic Regular Expressions
+print(tokens)
+
+# Declaration of Basic Regular Expressions
 t_EQUALS = r'\='
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
+t_LCURLYB = r'\{'
+t_RCURLYB = r'\}'
 t_DOT = r'\.'
 t_COMMA = r'\,'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 
+# SIP Regular Expressions Patterns
+reg_method_np = re.compile('|'.join(reserved['METHOD_NP']))
+reg_method_1p = re.compile('|'.join(reserved['METHOD_1P']))
+reg_method_2p = re.compile('|'.join(reserved['METHOD_2P']))
+reg_method_no = re.compile(reserved.get('METHOD_NO')[0])
+reg_level = re.compile('|'.join(reserved['LEVEL']))
+reg_direction = re.compile('|'.join(reserved['DIRECTION']))
 
-# Regular Expression
+# SIP Regular Expressions
+@TOKEN(reg_method_np.pattern)
+def t_METHOD_NP(t):
+    return t
+
+
+@TOKEN(reg_method_1p.pattern)
+def t_METHOD_1P(t):
+    return t
+
+
+@TOKEN(reg_method_2p.pattern)
+def t_METHOD_2P(t):
+    return t
+
+
+@TOKEN(reg_method_no.pattern)
+def t_METHOD_NO(t):
+    return t
+
+
+@TOKEN(reg_level.pattern)
+def t_LEVEL(t):
+    return t
+
+
+@TOKEN(reg_direction.pattern)
+def t_DIRECTION(t):
+    return t
+
+# Generic Regular Expressions
 
 def t_FLOAT(t):
     r'\d+\.\d'
@@ -60,11 +113,26 @@ def t_INT(t):
         t.value = 0
     return t
 
+def t_STRING(t):
+    r'\"(.+?)\"'
+    return t
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = 'ID'
+    # t.type = reserved.get(t.value, 'ID')  # Check reserved words
     return t
+
+# def t_LIST(t):
+#     r'[(][-?0-9,]+[-?0-9][)]'
+#     t.type = 'LIST'
+#     return t
+
+# def t_METHOD(t):
+#     r'[a-zA-Z_][a-zA-Z_0-9]*[(][)]'
+#     t.type = 'METHOD'
+#     return t
+
 
 # Ignored characters
 t_ignore = " \t"
@@ -82,7 +150,12 @@ def t_error(t):
 # Lexer
 lexer = lex.lex()
 
-lexer.input("img.readImage()")
+test1 = "greyScale translate readImage medium rotate low high \"Hello\" \'poop\'"
+test2 = "img.rotate(right)"
+test3 = "hello = readImage(\"Desktop\")"
+
+lexer.input(test3)
+
 
 # Looping for input
 while True:
